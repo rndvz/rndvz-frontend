8056b365d09f309ca96fc13d4f21995f31701a57
pmatejko <matejko.pawel@gmail.com>
2018-06-06 19:56:48 +0200

    added accidentaly removed component back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardComponent } from './components/card/card.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { LoginComponent } from './components/login/login.component';
-import { MainComponent } from './components/main/main.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserService } from './services/user.service';
 import { OcticonDirective } from './directives/octicon.directive';
-import { CardsComponent } from './components/cards/cards.component';
+import { UserService } from './services/user.service';
 
 
 const appRoutes = [
@@ -27,14 +27,14 @@ const appRoutes = [
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
+    NavbarComponent,
+    MainComponent,
     ChatComponent,
+    CardsComponent,
+    CardComponent,
     SettingsComponent,
     LoginComponent,
-    MainComponent,
-    NavbarComponent,
-    OcticonDirective,
-    CardsComponent
+    OcticonDirective
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,165 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AnimationBuilder, AnimationFactory, AnimationPlayer, keyframes, style, animate } from '@angular/animations';
+import { Person } from '../../util/person';
+import { Gender } from '../../util/gender.enum';
+import { PanHammerInput } from '../../util/pan-hammer-input';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+  @ViewChild('mainCard') public cardElement: ElementRef;
+  // @ViewChildren('secondaryCard') public cardElements: QueryList<ElementRef>;
+  public currentPlayer: AnimationPlayer;
+  public cards: Person[];
+  public maxCards = 10;
+
+  public startXPosition = 0;
+  public startYPosition = 0;
+  public startTransform = 'rotate(0deg)';
+  public currentXPosition = this.startXPosition;
+  public currentYPosition = this.startYPosition;
+  public currentTransform = this.startTransform;
+
+  constructor(private domSanitizer: DomSanitizer, private animationBuilder: AnimationBuilder) { }
+
+  ngOnInit() {
+    const photo1 = 'https://thumb9.shutterstock.com/display_pic_with_logo/111616/111616,1312478131,2/stock-photo-beauty-woman' +
+      '-portrait-of-teen-girl-beautiful-cheerful-enjoying-with-long-brown-hair-and-clean-skin-82225732.jpg';
+    const photo2 = 'https://thumb9.shutterstock.com/display_pic_with_logo/399136/646898302/stock-photo-beautiful-woman-646898302.jpg';
+    const card1 = new Person('Amelia', 'hehe ;)', Gender.FEMALE, [photo1]);
+    const card2 = new Person('Jessica', '(:', Gender.FEMALE, [photo2]);
+    this.cards = [card1, card2, card1];
+  }
+
+  onPanStart(event: PanHammerInput): void {
+    if (this.currentPlayer != null) {
+      this.resetCardElement();
+    }
+
+    this.startXPosition = this.cardElement.nativeElement.offsetLeft;
+    this.startYPosition = this.cardElement.nativeElement.offsetTop;
+    this.startTransform = 'rotate(0deg)';
+  }
+
+  private resetCardElement(): void {
+    this.currentPlayer.destroy();
+    this.currentXPosition = this.startXPosition;
+    this.currentYPosition = this.startYPosition;
+    this.currentTransform = this.startTransform;
+  }
+
+  onPanMove(event: PanHammerInput): void {
+    const offsetX = event.deltaX + this.startXPosition;
+    this.currentXPosition = offsetX;
+    this.currentYPosition = event.deltaY + this.startYPosition;
+    this.currentTransform = 'rotate(' + (((offsetX - this.startXPosition) * 50) / this.cardElement.nativeElement.clientWidth) + 'deg)';
+  }
+
+  onPanEnd(event: PanHammerInput): void {
+    if (this.isSwipeRight(event)) {
+      this.currentPlayer = this.createRightAnimation().create(this.cardElement.nativeElement);
+      this.currentPlayer.onDone( () => {
+        this.cards.shift();
+        this.currentXPosition = this.startXPosition;
+        this.currentYPosition = this.startYPosition;
+        this.currentTransform = this.startTransform;
+      });
+    } else if (this.isSwipeLeft(event)) {
+      this.currentPlayer = this.createLeftAnimation().create(this.cardElement.nativeElement);
+      this.currentPlayer.onDone(() => {
+        this.cards.shift();
+        this.currentXPosition = this.startXPosition;
+        this.currentYPosition = this.startYPosition;
+        this.currentTransform = this.startTransform;
+      });
+    } else {
+      this.currentPlayer = this.createReturnAnimation().create(this.cardElement.nativeElement);
+      this.currentPlayer.onStart(() => {
+        this.currentXPosition = this.startXPosition;
+        this.currentYPosition = this.startYPosition;
+        this.currentTransform = this.startTransform;
+      });
+    }
+
+    this.currentPlayer.play();
+  }
+
+  private isSwipeRight(event: PanHammerInput): boolean {
+    return (event.additionalEvent === 'panright' && event.velocityX > 3.0) ||
+      (event.deltaX > this.cardElement.nativeElement.clientWidth / 2.5);
+  }
+
+  private isSwipeLeft(event: PanHammerInput): boolean {
+    return (event.additionalEvent === 'panleft' && event.velocityX < -3.0) ||
+      (event.deltaX * (-1) > this.cardElement.nativeElement.clientWidth / 2.5);
+  }
+
+  private createReturnAnimation(): AnimationFactory {
+    return this.animationBuilder.build([
+      animate(150, keyframes([
+        style({
+          left: '*',
+          top: '*',
+          transform: '*'
+        }),
+        style({
+          left: this.startXPosition + 'px',
+          top: this.startYPosition + 'px',
+          transform: this.startTransform
+        })
+      ]))
+    ]);
+  }
+
+  private createRightAnimation(): AnimationFactory {
+    return this.animationBuilder.build([
+      animate(250, keyframes([
+        style({
+          left: '*',
+          top: '*',
+          transform: '*'
+        }),
+        style({
+          left: (window.innerWidth * 2) + 'px',
+          top: 0 + 'px',
+          transform: 'rotate(0deg)'
+        })
+      ])),
+      animate(1, keyframes([
+        style({
+          left: this.startXPosition + 'px',
+          top: this.startYPosition + 'px',
+          transform: this.startTransform
+        })
+      ]))
+    ]);
+  }
+
+  private createLeftAnimation(): AnimationFactory {
+    return this.animationBuilder.build([
+      animate(250, keyframes([
+        style({
+          left: '*',
+          top: '*',
+          transform: '*'
+        }),
+        style({
+          left: (-window.innerWidth * 2) + 'px',
+          top: 0 + 'px',
+          transform: 'rotate(0deg)'
+        })
+      ])),
+      animate(1, keyframes([
+        style({
+          left: this.startXPosition + 'px',
+          top: this.startYPosition + 'px',
+          transform: this.startTransform
+        })
+      ]))
+    ]);
+  }
+}
