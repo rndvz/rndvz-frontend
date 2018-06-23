ebc99577d9bec53ff99c883ff10681affb451549
mateusznajdek <mateusz14321@gmail.com>
2018-06-23 12:58:03 +0200

    Added password boxes to user registry

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -11,6 +11,7 @@ import {s} from '@angular/core/src/render3';
 export class RegisterComponent implements OnInit {
 
   login: string;
+  password: string;
   birthDate: Date;
   userSex: boolean;  // 0 -men 1 - women | can simply change to 3 posibilities (extra one initialized - unchecked)
   userSexPreference: boolean;  // same as above
@@ -22,19 +23,22 @@ export class RegisterComponent implements OnInit {
 
   submit(e) {
     e.preventDefault();
-    console.log(e);
-    this.login = e.target.elements[0].value;
-    this.birthDate = e.target.elements[1].value;
-    const userSexMale = e.target.elements[2].checked;
-    const userSexFemale = e.target.elements[3].checked;
-    const userSexPreferenceMale = e.target.elements[4].checked;
-    const userSexPreferenceFemale = e.target.elements[5].checked;
-    this.description = e.target.elements[6].value;
+    this.login =  e.target.querySelector('#loginNameInput').value;
+    const pass1 = e.target.querySelector('#passwordInput').value;
+    const pass2 = e.target.querySelector('#passsword2Input').value;
+    this.birthDate = e.target.querySelector('#birthdateInput').value;
+    const userSexMale = e.target.querySelector('#sexGenderMale').checked;
+    const userSexFemale = e.target.querySelector('#sexGenderFemale').checked;
+    const userSexPreferenceMale = e.target.querySelector('#sexPreferenceGenderMale').checked;
+    const userSexPreferenceFemale = e.target.querySelector('#sexPreferenceGenderFemale').checked;
+    this.description = e.target.querySelector('#infoInput').value;
 
     if (userSexMale) { this.userSex = false; } else if (userSexFemale) { this.userSex = true; }
     if (userSexPreferenceMale) { this.userSexPreference = false; } else if (userSexPreferenceFemale) { this.userSexPreference = true; }
 
-    if (!this.isLoginTaken(this.login)) {
+    if (!this.isLoginTaken(this.login) && pass1 === pass2) {
+      this.password = pass1;
+      console.log(this.login, this.password, this.birthDate, this.userSex, this.userSexPreference, this.description);
       this.registerUser();
     }
 
