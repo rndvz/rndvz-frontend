e342fa14b352ecf0c76ced13ce47ba13e9009444
mateusznajdek <mateusz14321@gmail.com>
2018-06-23 11:59:36 +0200

    Implemented parsing information from registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {Router} from '@angular/router';
+import {s} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,47 @@ import {Router} from '@angular/router';
 export class RegisterComponent implements OnInit {
 
   login: string;
-
+  birthDate: Date;
+  userSex: boolean;  // 0 -men 1 - women | can simply change to 3 posibilities (extra one initialized - unchecked)
+  userSexPreference: boolean;  // same as above
+  description: string;
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
 
-  registerUser(e) {
+  submit(e) {
     e.preventDefault();
     console.log(e);
     this.login = e.target.elements[0].value;
-    console.log(this.login);
+    this.birthDate = e.target.elements[1].value;
+    const userSexMale = e.target.elements[2].checked;
+    const userSexFemale = e.target.elements[3].checked;
+    const userSexPreferenceMale = e.target.elements[4].checked;
+    const userSexPreferenceFemale = e.target.elements[5].checked;
+    this.description = e.target.elements[6].value;
+
+    if (userSexMale) { this.userSex = false; } else if (userSexFemale) { this.userSex = true; }
+    if (userSexPreferenceMale) { this.userSexPreference = false; } else if (userSexPreferenceFemale) { this.userSexPreference = true; }
+
+    if (!this.isLoginTaken(this.login)) {
+      this.registerUser();
+    }
+
+    // console.log(this.userSex);
+    // console.log(this.userSexPreference);
+    // console.log(this.description );
     this.router.navigate(['/login']);
+  }
+
+
+
+  private registerUser() {
+    return true; // TODO registering user in backend
+  }
+
+  private isLoginTaken(login: string) {
+    return false;  // TODO checking if login is already taken
   }
 
 }
