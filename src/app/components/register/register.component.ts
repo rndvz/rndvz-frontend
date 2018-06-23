4ce608b58dec7dff9fabe71d883c0b2c915ba673
mateusznajdek <mateusz14321@gmail.com>
2018-06-23 06:44:49 +0200

    User Register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  login: string;
+
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
 
   registerUser(e) {
     e.preventDefault();
-
+    console.log(e);
+    this.login = e.target.elements[0].value;
+    console.log(this.login);
+    this.router.navigate(['/login']);
   }
 
 }
